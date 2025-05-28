266022acb0879d2456fb54f1715fcb57afc16ab3
SonamPhuntshok <sonammmm2001@gmail.com>
2025-05-28 12:05:28 +0600

    UserAuth

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { isAuthenticated } = require('../middleware/authMiddleware');
+const { isAuthenticated } = require('../Middleware/authMiddleware');
 const userController = require('../controller/userController');
 
 
